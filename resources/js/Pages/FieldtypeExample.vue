ae9a47f87e7b8eaa73c119aba24630ed38ae6542
Michel <mj.99@live.nl>
2022-08-31 11:10:45 +0200

    FIELDTYPE COMPONENTS WORK!

---
+++ resources/js/Pages/FieldtypeExample.vue
@@ -0,0 +1,28 @@
+<template>
+    <dashboard>
+        Peop
+        <component v-if="comp" :is="comp"/>
+    </dashboard>
+</template>
+<script>
+let test = document.body.getAttribute('data-test');
+
+import Dashboard from "../Layouts/Dashboard.vue";
+
+export default {
+    components: {Dashboard},
+
+    data() {
+        return {
+            comp: null
+        };
+    },
+
+    async beforeCreate()
+    {
+        const Test = await import(test);
+        console.log(Test.default, Dashboard)
+        this.comp = Test.default;
+    }
+}
+</script>
