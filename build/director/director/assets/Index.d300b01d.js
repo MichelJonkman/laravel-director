6be73a014e21eb2abba313f0dd26152af137c9b9
Michel <mj.99@live.nl>
2022-09-23 13:29:09 +0200

    Fixed app.scss not being loaded

---
+++ build/director/director/assets/Index.d300b01d.js
@@ -0,0 +1,1 @@
+import{_ as o,D as t}from"./Dashboard.b77415e8.js";import{r as s,o as n,c as a,w as _,d as r}from"./app.b7daca6a.js";const c={components:{Dashboard:t}},d=r(" Peops ");function p(l,f,i,m,u,x){const e=s("Dashboard");return n(),a(e,null,{default:_(()=>[d]),_:1})}const $=o(c,[["render",p]]);export{$ as default};
