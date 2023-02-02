08d513d26342d1861ec1b460f5ac06eb0f485608
MichelJonkman <mj.99@live.nl>
2023-02-02 19:36:46 +0100

    Moved migrations up one level

--- src/Providers/DatabaseServiceProvider.php
+++ src/Providers/DatabaseServiceProvider.php
@@ -8,6 +8,6 @@ class DatabaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
     }
 }
