87b055ed0174c14c032d0a8ac75f59cf7eb363b7
michel <mj.99@live.nl>
2023-08-03 10:20:28 +0200

    Started restyling sidebar menu

--- resources/package/js/Components/Layout/Dashboard/Sidebar.vue
+++ resources/package/js/Components/Layout/Dashboard/Sidebar.vue
@@ -1,26 +1,11 @@
 <template>
-    <div class="sidebar d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-        <a href="/admin/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <span class="fs-4">Director</span>
-        </a>
-        <hr>
-        <Nav :menu="menu"/>
-        <hr>
-        <div class="dropdown">
-            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                <strong>mdo</strong>
+    <div class="sidebar d-flex flex-column flex-shrink-0 text-white bg-dark" style="width: 280px;">
+        <div class="sidebar-brand">
+            <a href="/admin/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <span class="fs-4">Director</span>
             </a>
-            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                <li><a class="dropdown-item" href="#">New project...</a></li>
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Sign out</a></li>
-            </ul>
         </div>
+        <Nav :menu="menu"/>
     </div>
 </template>
 
@@ -35,6 +20,16 @@ let menu: MenuInterface = pageProps.menu as MenuInterface;
 
 <style lang="scss" scoped>
 .sidebar {
-    height: 100%;
+    $topbar-height: 4rem;
+
+    height:         100%;
+
+    .sidebar-brand {
+        background: $dark-blue-900;
+        height:     $topbar-height;
+        display: flex;
+        justify-content: center;
+        padding: 1rem;
+    }
 }
 </style>
